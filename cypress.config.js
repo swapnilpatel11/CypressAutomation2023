58e80ac3fe43e998cb97e50dccf1e5934ad8dfb7
Swapnil Patel <sappu004@gmail.com>
2023-09-24 18:52:46 -0400

    Complete Installation of BDD

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("cypress");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", preprocessor(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   projectId: "4fua5j",
@@ -7,10 +23,8 @@ module.exports = defineConfig({
     url : "https://rahulshettyacademy.com"
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
     //provide path of testcases
-    specPattern: 'cypress/integration/'
+    specPattern: 'cypress/integration/examples/BDD/*.feature'
   },
 });
